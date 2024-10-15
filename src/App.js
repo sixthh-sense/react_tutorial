394e30e621b62c42059fd70d7d496c794a52431b
haein_sung <blue6zombie6@gmail.com>
2024-10-15 10:28:40 +0900

    #24 (key 변수 추가. db pk 비슷)

--- src/App.js
+++ src/App.js
@@ -13,14 +13,20 @@ function App() {
     ])
 
     const addTodo = (description, assigned) => {
-      if (todos.length > 0) {
-          const newTodo = {
-              rowNumber: todos.length + 1,
-              rowDescription: description,
-              rowAssigned: assigned
-          };
-          setTodos(todos => [...todos, newTodo]);
-      }
+        let rowNumber = 0;
+
+        if (todos.length > 0) {
+            rowNumber = todos[todos.length - 1].rowNumber + 1;
+        } else {
+            rowNumber = 1;
+        }
+
+        const newTodo = {
+            rowNumber: rowNumber,
+            rowDescription: description,
+            rowAssigned: assigned
+        };
+        setTodos(todos => [...todos, newTodo]);
     }
 
   return (
