eeb81eb2a93dbe358d3fd2308f3815e2599e7ee0
haein_sung <blue6zombie6@gmail.com>
2024-10-15 09:47:50 +0900

    #20 (useState로 화면 rerendering)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
+import React, {useState} from 'react';
 import TodoTable from './components/TodoTable';
 import './App.css';
 
 function App() {
 
-  const todos = [
+    const [todos, setTodos] = useState([
       {rowNumber: 1, rowDescription: 'Feed puppy', rowAssigned: 'User One'},
       {rowNumber: 2, rowDescription: 'Water plants', rowAssigned: 'User Two'},
       {rowNumber: 3, rowDescription: 'Make dinner', rowAssigned: 'User One'},
       {rowNumber: 4, rowDescription: 'Charge phone battery', rowAssigned: 'User One'}
-  ]
+    ])
 
     const addTodo = () => {
       if (todos.length > 0) {
@@ -17,8 +18,7 @@ function App() {
               rowDescription: 'New Todo',
               rowAssigned: 'User Three'
           };
-          todos.push(newTodo);
-          console.log(todos);
+          setTodos(todos => [...todos, newTodo]);
       }
     }
 
